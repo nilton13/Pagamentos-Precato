9835a43d5379dc3ea817b11675457a85a768f29f
NIlton <jnvlfilho@hotmail.com>
2021-11-14 16:42:06 -0300

    Customizando o tratamento de erros.

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,13 +1,28 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '../errors/AppError';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use(routes)
+app.use(routes);
+
+app.use((error: Error, resquest: Request, response: Response, next: NextFunction) =>{
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            status: 'error',
+            message: error.message
+        })
+    }
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server Error',
+    })
+})
 
 app.listen(3333 , () => {
     console.log("Server is started on PORT 3333!");
